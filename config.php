b1c693f1360986e6abbb47cf0995b749bf575f2e
Mtauqeer-dev <tauqeerch03@gmail.com>
2025-05-08 05:31:52 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'instamedia.mysql.database.azure.com';
-$user = 'mtau';        // default XAMPP user
-$password = '@T@uqeer11@';        // default password is empty
-$database = 'instagram_photo';
+$host = "instamedia.mysql.database.azure.com";
+$user = "mtau";        // default XAMPP user
+$password = "@T@uqeer11@";        // default password is empty
+$database = "instagram_photo";
 
 $conn = new mysqli($host, $user, $password, $database);
 
